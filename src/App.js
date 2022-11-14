7e6b374f3719d3eb8a630cd55db921a1a286d4be
sophia0110 <njjp6ru@yahoo.com.tw>
2022-11-14 22:44:28 +0800

    push

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h3>hi 123</h3>
-      <h3>hi 123456</h3>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <>
-    <h3>hi 123</h3>
+  <React.StrictMode>
     <App />
-  </>
-  // </React.StrictMode>
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
